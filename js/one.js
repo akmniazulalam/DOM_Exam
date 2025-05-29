62081da287d57f281143c0b8f01bf83262f3fe0f
niazulalam <niazulalam097@gmail.com>
2025-05-29 18:01:24 +0600

    One Item Create in JavaScript

--- js/one.js
+++ js/one.js
@@ -1,11 +1,15 @@
 let input = document.getElementById("input");
 let addBtn = document.getElementById("addBtn");
-let ul = document.getElementById("ul");
+let form = document.getElementById("form");
+
 
 addBtn.addEventListener("click", () => {
   if (input.value == "") {
     alert("Please Enter A Value");
-  } else {
+  } 
+  else {
+    let ul = document.createElement("ul");
+    form.appendChild(ul)
     let li = document.createElement("li");
     li.innerHTML = input.value;
     ul.appendChild(li);
